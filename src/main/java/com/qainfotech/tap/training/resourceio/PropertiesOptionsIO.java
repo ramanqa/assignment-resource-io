522edb653a4a7a560fe6287525489a99ccaabddd
SomilBansalQA <somilbansal@qainfotech.com>
2017-06-27 09:23:41 +0530

    UpdatePropertiesOptionsIO.java
    Added a code in method.

--- src/main/java/com/qainfotech/tap/training/resourceio/PropertiesOptionsIO.java
+++ src/main/java/com/qainfotech/tap/training/resourceio/PropertiesOptionsIO.java
@@ -9,10 +9,27 @@ import java.io.IOException;
 public class PropertiesOptionsIO{
     
     public Object getOptionValue(String optionKey) throws IOException {
-        throw new UnsupportedOperationException("Not implemented.");
+      //  throw new UnsupportedOperationException("Not implemented.");
+     FileReader reader=new FileReader("C:\\Users\\somilbansal\\Desktop\\Eclipse_Workspace\\assignment-resource-io-master1\\src\\test\\resources\\options.properties"); 
+
+    	Properties p=new Properties(); 
+    	p.load(reader); 
+
+    	return p.getProperty(optionKey);
     }
 
     public void addOption(String optionKey, Object optionValue) throws IOException {
-        throw new UnsupportedOperationException("Not implemented.");
+     //   throw new UnsupportedOperationException("Not implemented.");
+    
+        FileReader reader=new FileReader("C:\\Users\\somilbansal\\Desktop\\Eclipse_Workspace\\assignment-resource-io-master1\\src\\test\\resources\\options.properties"); 
+
+    	Properties p=new Properties(); 
+    	p.load(reader); 
+
+    	p.setProperty(optionKey,(String) optionValue);
+
+    	p.store(new FileWriter("C:\\Users\\somilbansal\\Desktop\\Eclipse_Workspace\\assignment-resource-io-master1\\src\\test\\resources\\options.properties"),"Written");
+
+    
     }
 }
