7af0f1d73921d34d79f7cb9cbc9a6dec6679b9bd
nishant-qait <nishantagarwal@qainfotech.com>
2017-06-27 09:25:32 +0530

    Update Individual.java

--- src/main/java/com/qainfotech/tap/training/resourceio/model/Individual.java
+++ src/main/java/com/qainfotech/tap/training/resourceio/model/Individual.java
@@ -1,44 +1,51 @@
 package com.qainfotech.tap.training.resourceio.model;
 
-import java.util.Map;
+import java.io.FileReader;
+import java.util.*;
 
 /**
  *
  * @author Ramandeep RamandeepSingh AT QAInfoTech.com
  */
 public class Individual {
-    
-    private final String name;
-    private final Integer id;
-    private final Boolean active;
-    
-    public Individual(Map<String, Object> individualMap){
-        throw new UnsupportedOperationException("Not implemented.");
-    }
-    
-    /**
-     * get the name of individual
-     * 
-     * @return individual name
-     */
-    public String getName(){
-        return name;
-    }
-    
-    /**
-     * get the employee of of individual
-     * @return id
-     */
-    public Integer getId(){
-        return id;
-    }
-    
-    /**
-     * get the active status of individual
-     * 
-     * @return 
-     */
-    public Boolean isActive(){
-        return active;
-    }
+
+	private final String name;
+	private final Integer id;
+	private final Boolean active;
+
+	public Individual(Map<String, Object> individualMap) {
+		// throw new UnsupportedOperationException("Not implemented.");
+
+		this.name = (String) individualMap.get("name");
+		this.id = (Integer) individualMap.get("id");
+		this.active = (Boolean) individualMap.get("active");
+
+	}
+
+	/**
+	 * get the name of individual
+	 * 
+	 * @return individual name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * get the employee of of individual
+	 * 
+	 * @return id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * get the active status of individual
+	 * 
+	 * @return
+	 */
+	public Boolean isActive() {
+		return active;
+	}
 }
