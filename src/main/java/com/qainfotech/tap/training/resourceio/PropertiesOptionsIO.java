55c37279217f92dbc134d7b81b541271b46772f4
nishant-qait <nishantagarwal@qainfotech.com>
2017-06-28 09:51:03 +0530

    Update PropertiesOptionsIO.java

--- src/main/java/com/qainfotech/tap/training/resourceio/PropertiesOptionsIO.java
+++ src/main/java/com/qainfotech/tap/training/resourceio/PropertiesOptionsIO.java
@@ -1,7 +1,12 @@
 package com.qainfotech.tap.training.resourceio;
 
-import java.io.*;
-import java.util.Properties;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.*;
 
 /**
  *
@@ -10,63 +15,34 @@ import java.util.Properties;
 public class PropertiesOptionsIO {
 
 	public Object getOptionValue(String optionKey) throws IOException {
-
-		Properties prop = new Properties();
-		InputStream input = null;
-		Object obj = null;
-
-		File file;
+		// throw new UnsupportedOperationException("Not implemented.");
 		try {
 
-			input = new FileInputStream("src/main/resources/options.properties");
+			Properties prop = new Properties();
+			InputStream input = new FileInputStream("src/main/resources/options.properties");
 			prop.load(input);
-			obj = prop.getProperty(optionKey);
-
-		} catch (IOException ex) {
-			ex.printStackTrace();
+			return ((Object) prop.getProperty(optionKey));
+		} catch (Exception e) {
+			System.out.println(e);
 		}
-		return obj;
-
+		return null;
 	}
 
 	public void addOption(String optionKey, Object optionValue) throws IOException {
+		// throw new UnsupportedOperationException("Not implemented.");
 
 		Properties prop = new Properties();
-		FileOutputStream output = null;
-		File file, filecopy;
-
-		String optionKey1 = "ResourceIOTest";
-		String optionValue1 = "";
-
-		String optionKey2 = "TestName";
-		String OptionValue2 = "";
-
-		String optionKey3 = "TimeStamp";
-		String OptionValue3 = "";
-
+		File file;
 		InputStream input = null;
-		String obj = null;
+		OutputStream output = null;
 
 		try {
-
 			file = new File("src/main/resources/options.properties");
-			filecopy = file;
-
-			input = new FileInputStream("src/main/resources/options.properties");
-
-			prop.load(input);
-
+			input = new FileInputStream(file);
+			prop.load(input);			
 			output = new FileOutputStream(file);
-
-			prop.load(new FileInputStream("src/main/resources/options.properties"));
-
-			optionValue1 = prop.getProperty(optionKey1);
-			OptionValue2 = prop.getProperty(optionKey2);
-
-			prop.setProperty(optionKey1, optionValue1);
-			prop.setProperty(optionKey2, OptionValue2);
 			prop.setProperty(optionKey, optionValue.toString());
-
+ 
 			prop.store(output, null);
 
 		} catch (IOException io) {
@@ -75,5 +51,6 @@ public class PropertiesOptionsIO {
 		} finally {
 			output.close();
 		}
+
 	}
 }
