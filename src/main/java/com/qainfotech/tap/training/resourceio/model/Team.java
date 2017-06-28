b7a49af81579be55ffdd78562e0f7ddccafffc91
nishant-qait <nishantagarwal@qainfotech.com>
2017-06-28 09:51:55 +0530

    Update Team.java

--- src/main/java/com/qainfotech/tap/training/resourceio/model/Team.java
+++ src/main/java/com/qainfotech/tap/training/resourceio/model/Team.java
@@ -1,15 +1,6 @@
 package com.qainfotech.tap.training.resourceio.model;
 
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import java.util.*;
 
 import com.qainfotech.tap.training.resourceio.TeamsJsonReader;
 
@@ -17,38 +8,18 @@ import com.qainfotech.tap.training.resourceio.TeamsJsonReader;
  *
  * @author Ramandeep RamandeepSingh AT QAInfoTech.com
  */
-
-
-/*
-working on following class
-*/
 public class Team {
 
 	private final String name;
 	private final Integer id;
-	private final List<Individual> members = new ArrayList<Individual>();
-
-	Individual individual = null;
-	Map<String, Object> myMap = null;
+	private final List<Individual> members;
 
 	public Team(Map<String, Object> teamMap) {
-		// throw new UnsupportedOperationException("Not implemented.");
-
-		myMap = new HashMap<String, Object>();
-
-		individual = new Individual(myMap);
 
 		this.name = (String) teamMap.get("name");
 		this.id = (Integer) teamMap.get("id");
-
-		// members.add((Individual)teamMap.get("members"));
-
-		// this.members.addAll((Individual)teamMap.get("members"));
-
-		members.add((Individual) teamMap.get("members"));
-
-		// this.active = (Boolean)teamMap.get("members");
-
+		List<Individual> list = (List<Individual>) teamMap.get("members");
+		this.members = list;
 	}
 
 	/**
@@ -85,82 +56,18 @@ public class Team {
 	 * @return
 	 */
 	public List<Individual> getActiveMembers() {
-		// throw new UnsupportedOperationException("Not implemented.");
 
-		List<Team> teamList = new ArrayList();
+		List<Individual> individualList;
+		List<Individual> activeIndividualList = new ArrayList<>();
 
-		Team team = null;
-
-		Object obj = null;
-		JSONObject jsonObject = null;
-		JSONArray jsonArray = null;
-		List<String> list = null;
-		JSONObject jsonObject2 = null;
-		Map<String, Object> myMap = null;
-
-		List<Individual> members = new ArrayList<Individual>();
-
-		Map<String, Object> myMapInd = null;
-		System.out.println("lol");
-		Individual individualObject;
-
-		TeamsJsonReader red = new TeamsJsonReader();
-
-		try {
-			obj = (new JSONParser()).parse(new FileReader("src/main/resources/db.json"));
-			jsonObject = (JSONObject) obj;
-			jsonArray = (JSONArray) jsonObject.get("teams");
-
-			list = new ArrayList<String>();
-
-			for (int i = 0; i < jsonArray.size(); i++) {
-				jsonObject2 = (JSONObject) jsonArray.get(i);
-
-				System.out.println((String) jsonObject2.get("name") + "   here");
-
-				myMap = new HashMap<String, Object>();
-
-				myMap.put("name", jsonObject2.get("name"));
-				myMap.put("id", ((Long) jsonObject2.get("id")).intValue());
-
-				JSONArray memberArray = (JSONArray) jsonObject2.get("members");
-
-				System.out.println(memberArray);
-
-				// Object myObj = memberArray.get(1);
-				// System.out.println(myObj);
-
-				// System.out.println((JSONObject)memberArray.get(0)+"here
-				// also");
-
-				// System.out.println(myObj.get(0)+"here also");
-
-				for (int j = 0; j < memberArray.size(); j++) {
-
-					Object myObj = memberArray.get(j);
-
-					individualObject = (Individual) red.getIndividualById(((Long) myObj).intValue());
-
-					members = red.getListOfActiveIndividuals();
-
-					myMap.put("members", individualObject);
-					// myMap.put("members", jsonObject2.get("members"));
-
-					System.out.println("here also");
-					// members.add(individualObject);
-				}
-
-				team = new Team(myMap);
-
-				teamList.add(team);
-
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
+		individualList = this.getMembers();
+		//System.out.println(individualList.size());
+		for (Individual e : individualList) {
+			if (e.isActive().compareTo(true) == 0)
+				activeIndividualList.add(e);
 		}
 
-		return members;
+		return activeIndividualList;
 
 	}
 
@@ -170,81 +77,17 @@ public class Team {
 	 * @return
 	 */
 	public List<Individual> getInactiveMembers() {
-		// throw new UnsupportedOperationException("Not implemented.");
+		List<Individual> individualList;
+		List<Individual> inactiveIndividualList = new ArrayList<>();
 
-		List<Team> teamList = new ArrayList();
+		individualList = this.getMembers();
 
-		Team team = null;
+		for (Individual e : individualList) {
+			if (e.isActive().compareTo(false) == 0)
+				inactiveIndividualList.add(e);
 
-		Object obj = null;
-		JSONObject jsonObject = null;
-		JSONArray jsonArray = null;
-		List<String> list = null;
-		JSONObject jsonObject2 = null;
-		Map<String, Object> myMap = null;
-
-		List<Individual> members = new ArrayList<Individual>();
-
-		Map<String, Object> myMapInd = null;
-		System.out.println("lol");
-		Individual individualObject;
-
-		TeamsJsonReader red = new TeamsJsonReader();
-
-		try {
-			obj = (new JSONParser()).parse(new FileReader("src/main/resources/db.json"));
-			jsonObject = (JSONObject) obj;
-			jsonArray = (JSONArray) jsonObject.get("teams");
-
-			list = new ArrayList<String>();
-
-			for (int i = 0; i < jsonArray.size(); i++) {
-				jsonObject2 = (JSONObject) jsonArray.get(i);
-
-				System.out.println((String) jsonObject2.get("name") + "   here");
-
-				myMap = new HashMap<String, Object>();
-
-				myMap.put("name", jsonObject2.get("name"));
-				myMap.put("id", ((Long) jsonObject2.get("id")).intValue());
-
-				JSONArray memberArray = (JSONArray) jsonObject2.get("members");
-
-				System.out.println(memberArray);
-
-				// Object myObj = memberArray.get(1);
-				// System.out.println(myObj);
-
-				// System.out.println((JSONObject)memberArray.get(0)+"here
-				// also");
-
-				// System.out.println(myObj.get(0)+"here also");
-
-				for (int j = 0; j < memberArray.size(); j++) {
-
-					Object myObj = memberArray.get(j);
-
-					individualObject = (Individual) red.getIndividualById(((Long) myObj).intValue());
-
-					members = red.getListOfInactiveIndividuals();
-
-					myMap.put("members", individualObject);
-					// myMap.put("members", jsonObject2.get("members"));
-
-					System.out.println("here also");
-					// members.add(individualObject);
-				}
-
-				team = new Team(myMap);
-
-				teamList.add(team);
-
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
 
-		return members;
+		return inactiveIndividualList;
 	}
 }
